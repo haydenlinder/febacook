06165d92683fa7cc7c66bf0d8bda7c46fe139266
haydenlinder <haydenlinder@gmail.com>
2020-03-03 11:08:27 -0800

    add users reducer

--- febacook/frontend/reducers/users_reducer.js
+++ febacook/frontend/reducers/users_reducer.js
@@ -1,9 +1,18 @@
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_USERS } from "../actions/user_actions";
+
 const usersReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state);
     switch (action.value) {
-        case "banana":
-            
-            break;
-    
+        case RECEIVE_CURRENT_USER:
+            nextState[action.user.id] = action.user;
+            return nextState
+        case RECEIVE_USERS:
+            return action.users;
+        case REMOVE_USER:
+            delete nextState[action.id];
+            return nextState;
         default:
             return state;
     }
