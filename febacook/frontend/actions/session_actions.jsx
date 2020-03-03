618ec531ad9c593cb4a41b05a450944fac581576
haydenlinder <haydenlinder@gmail.com>
2020-03-03 09:14:38 -0800

    add App component

---
+++ febacook/frontend/actions/session_actions.jsx
@@ -0,0 +1,3 @@
+const createSession = credentials => dispatch => (
+
+)
---
+++ febacook/frontend/reducers/users_reducer.js
@@ -0,0 +1,12 @@
+const usersReducer = (state = {}, action) => {
+    switch (action.value) {
+        case "banana":
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
+
+export default usersReducer;
