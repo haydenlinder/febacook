3958ef4ee5154f80996c59c6127439e60f27b85d
haydenlinder <haydenlinder@gmail.com>
2020-03-04 00:09:42 -0800

    add styling

--- febacook/frontend/components/session_form.jsx
+++ febacook/frontend/components/session_form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createSession } from '../actions/session_actions';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
 
 class SessionForm extends React.Component {
     constructor(props) {
@@ -18,29 +19,46 @@ class SessionForm extends React.Component {
 
     render(){
         return(
-            <div>
-                <div class="logo">febacook</div>
-                <form>
-                    Email
-                    < input
-                        type = "text"
-                        value = { this.state.email }
-                        onChange = {
-                            (e) => this.setState({ email: e.target.value })
-                        }
-                        />
-                    < br />
+            <div class="logged-out-header">
+                <Link className="logo" to="/"> febacook </Link>
+        
+                <form class="session-form">
 
-                    Password
-                    < input
-                        type = "text"
-                        value = { this.state.password }
-                        onChange = {
-                            (e) => this.setState({ password: e.target.value })
-                        }
-                    />
-                    <br/> {this.props.errors} <br/>
-                    <button onClick={this.handleSubmit}>Log In</button>
+                    <div class="email"> Email 
+                        < input
+                            type = "text"
+                            value = { this.state.email }
+                            onChange = {
+                                (e) => this.setState({ 
+                                    email: e.target.value 
+                                })
+                            }
+                        />
+                    </div>
+
+                    <div class="password"> Password 
+                        < input
+                            type = "text"
+                            value = { this.state.password }
+                            onChange = {
+                                (e) => this.setState({ 
+                                    password: e.target.value 
+                                })
+                            }
+                        />
+                    </div>
+
+                    
+                    <div class="login-errors">
+                        {this.props.errors}
+                    </div> 
+                    
+                    <button 
+                        class="login"
+                        onClick={this.handleSubmit}
+                    >
+                        Log In
+                    </button>
 
                 </form>
             </div>
