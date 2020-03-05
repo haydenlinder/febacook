ec7fb140caa6226ec9719d58f9372c5e7a752286
haydenlinder <haydenlinder@gmail.com>
2020-03-04 17:32:32 -0800

    fix changes

---
+++ febacook/frontend/components/session_form.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { createSession } from '../actions/session_actions';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            password: "", email: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createSession(this.state)
+    }
+
+    render(){
+        return(
+            <div className="logged-out-header">
+                    <Link className="logo" to="/"> febacook </Link>
+            
+                    <form className="session-form">
+
+                        <div className="email"> 
+                            <div>Email</div> 
+                            < input
+                                type = "text"
+                                value = { this.state.email }
+                                onChange = {
+                                    (e) => this.setState({ 
+                                        email: e.target.value 
+                                    })
+                                }
+                            />
+                        </div>
+
+                        <div className="password"> 
+                            <div>Password</div> 
+                            < input
+                                type = "text"
+                                value = { this.state.password }
+                                onChange = {
+                                    (e) => this.setState({ 
+                                        password: e.target.value 
+                                    })
+                                }
+                            />
+                        </div>
+
+                        
+                        <div className="login-errors">
+                            {this.props.errors}
+                        </div> 
+                        
+                        <button 
+                            className="login"
+                            onClick={this.handleSubmit}
+                        >
+                            Log In
+                        </button>
+
+                    </form>
+            </div>
+        )
+    }
+}
+
+const msp = state => ({
+    errors: state.errors.session.login
+})
+
+const mdp = dispatch => ({
+    createSession: (credentials) => dispatch(createSession(credentials))
+})
+
+const SessionFormContainer = connect(msp, mdp)(SessionForm);
+
+export default SessionFormContainer;
---
+++ febacook/frontend/entry.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store'
+import Root from './components/root';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root")
+
+    let store;
+    if (window.currentUser) {
+        const { currentUser } = window;
+        const { id } = currentUser;
+        const preloadedState = {
+            entities: {
+                users: {
+                    [id]: currentUser
+                }
+            },
+            session: { id: id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
+    ReactDOM.render( <Root store={store}/>, root )
+})
